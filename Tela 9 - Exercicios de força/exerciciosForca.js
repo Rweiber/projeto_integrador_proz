285f4aafb0367f61d6f95dd93ce70d76567f7b3a
Renan <hideoutrws2@gmail.com>
2023-10-06 21:29:05 -0300

    carrosel treino força com js para botoes

--- Tela 9 - Exercicios de força/exerciciosForca.js
+++ Tela 9 - Exercicios de força/exerciciosForca.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    var repsButtons = document.querySelectorAll('.increase-reps, .decrease-reps');
-    var loadButtons = document.querySelectorAll('.increase-load, .decrease-load');
+    var repsButtons = document.querySelectorAll('.increase-reps1, .decrease-reps1');
+    var loadButtons = document.querySelectorAll('.increase-load1, .decrease-load1');
 
     repsButtons.forEach(function(btn) {
         btn.addEventListener('click', function() {
             var input = this.parentElement.querySelector('input[id^="reps"]');
             var currentVal = parseInt(input.value, 10);
-            if (this.classList.contains('increase-reps')) {
+            if (this.classList.contains('increase-reps1')) {
                 input.value = currentVal + 1;
             } else {
                 input.value = Math.max(0, currentVal - 1);
@@ -18,12 +18,13 @@ window.onload = function() {
         btn.addEventListener('click', function() {
             var input = this.parentElement.querySelector('input[id^="load"]');
             var currentVal = parseInt(input.value, 10);
-            if (this.classList.contains('increase-load')) {
-                input.value = currentVal + 1;
+            if (this.classList.contains('increase-load1')) {
+                input.value = currentVal + 10;
             } else {
-                input.value = Math.max(0, currentVal - 1);
+                input.value = Math.max(0, currentVal - 10);
             }
         });
     });
 };
+
 
