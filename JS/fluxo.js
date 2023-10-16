be7738d1139083305e96e9fbc5a89fd5f54a737f
Brenner <lucianobrenner07@gmail.com>
2023-10-16 19:28:48 -0300

    Adjusting at function

--- JS/fluxo.js
+++ JS/fluxo.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
       labels: ["100", "80", "60", "40", "20", "0"],
       datasets: [{
         label: "Fluxo de Pessoas",
-        hora: [06:00, 08:00, 10:00, 12:00, 14:00, 16:00, 18:00, 20:00, 22:00],
+        hora: ["06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
         backgroundColor: "rgba(75, 192, 192, 0.5)",
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1
@@ -26,4 +26,4 @@ document.addEventListener("DOMContentLoaded", function() {
       hora: dados,
       options: opcoes
     });
-  );
+  });
